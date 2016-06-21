b5363339db03f54cd033a15879c9a858e1988082
Jeff Corpac <jcorpac@gmail.com>
2016-06-21 15:57:13 -0500

    Initial Commit

---
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.sunset;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunsetActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
